5d310f18ba6a88f195ba3dbde9198f5eb3dba9e1
kokuto821 <82218969+kokuto821@users.noreply.github.com>
2021-06-02 20:41:39 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>あああ</li>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
+};
